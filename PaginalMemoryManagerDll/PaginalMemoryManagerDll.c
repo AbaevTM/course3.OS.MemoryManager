b361f9d340a56c2e114fb1cf4574eb86fe0751aa
VitaliPark <vitalipark@gmail.com>
2014-04-21 23:59:09 +0300

    malloc

--- PaginalMemoryManagerDll/PaginalMemoryManagerDll.c
+++ PaginalMemoryManagerDll/PaginalMemoryManagerDll.c
@@ -74,15 +74,96 @@
 	return 0;
  }
 
+ static MemoryBlock * findFreeBlock(size_t size){
+	 MemoryBlock *iterator = memoryManager.firstMemoryBlock; 	
+	 while(iterator != NULL){
+		 if(iterator->isFree && iterator->blockSize >= size){
+			return iterator;
+		 }
+	 }
+	 return NULL;
+ }
+
+ static void switchBlocks(MemoryBlock *freeBlock, MemoryBlock * newBlock){
+	freeBlock->previous->next = newBlock;
+	newBlock->previous = freeBlock->previous;
+	newBlock->next = freeBlock;
+	freeBlock->previous = newBlock;
+ }
+
+ static MemoryBlock * getBlock(VirtualAddres adress){
+	 MemoryBlock *iterator = memoryManager.firstMemoryBlock;
+	 while(iterator != NULL){
+		 if(iterator->blockAdress == adress){
+			 return iterator;
+		 }
+	 }
+	 return NULL;
+ }
+
 
  // Ёкспортируемые функции
- int _malloc (VirtualAddres ptr, size_t szBlock){
+int _malloc (VirtualAddres ptr, size_t szBlock){
+	MemoryBlock * freeBlock;
+	MemoryBlock * newBlock;
+	VirtualAddres adress;
 	printf("malloc\n");
+	if(!szBlock){
+		return -1;
+	}
+	freeBlock = findFreeBlock(szBlock);
+	if(!freeBlock){
+		return -2;
+	}
+	newBlock = (MemoryBlock *)malloc(sizeof(MemoryBlock));
+	adress = freeBlock->blockAdress;
+	freeBlock->blockAdress = (VirtualAddres)newBlock;
+	freeBlock->blockSize = freeBlock->blockSize - szBlock;
+	newBlock->blockAdress = adress;
+	newBlock->blockSize = szBlock;
+	newBlock->isFree = false;
+	if(!(freeBlock->previous)){
+		newBlock->next = freeBlock;
+		freeBlock->previous = newBlock;
+		newBlock->previous = NULL;
+		memoryManager.firstMemoryBlock = newBlock;
+	}
+	switchBlocks(freeBlock, newBlock);
+	ptr = (VirtualAddres)newBlock;
 	return 0;
 }
 
+
  int _free (VirtualAddres ptr){
+	MemoryBlock * deletedBlock;
+	MemoryBlock *temp;
 	printf("free\n");
+	/*deletedBlock = getBlock(ptr);
+	if(!deletedBlock){
+		return -1;	 
+	}
+	//if dissmised block is first and next one is not free
+	if(deletedBlock->previous == NULL && !deletedBlock->next->isFree){
+		deletedBlock->isFree = true;
+		return 0;
+	}
+	// -//- is first and the next one is free
+	if(deletedBlock->previous == NULL && deletedBlock->next){
+		temp = deletedBlock->next;
+		temp->blockSize = temp->blockSize + deletedBlock->blockSize;
+		temp->blockAdress = deletedBlock->blockAdress;
+		temp->previous = NULL;
+		memoryManager.firstMemoryBlock = temp;
+		free(deletedBlock);
+		return 0;
+	}
+	// -//- is not first and next one is free and previous one is not
+	if(deletedBlock->next->isFree && !deletedBlock->previous->isFree){
+		temp = deletedBlock->next;
+		temp->blockSize = temp->blockSize + deletedBlock->blockSize;
+		temp->blockAdress = deletedBlock->blockAdress;
+		temp->previous = deletedBlock->previous;
+	}*/
 	return 0;
 }
 
